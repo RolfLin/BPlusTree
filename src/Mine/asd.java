c1ebb720c97a6f466f99a5aeb32e9e058c29fa63
Bill Lin <billlin@BilldeMacBook-Pro.local>
2017-09-06 16:41:28 +0800

    Create a BPlusTree model

---
+++ src/Mine/asd.java
@@ -0,0 +1,9 @@
+package Mine;
+
+public class asd {
+    public static void main(String[] args) {
+        String str = "// ^ ^ ^/^n";
+        int a = str.length();
+        System.out.println(3/2);
+    }
+}
