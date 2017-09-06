49aa883722e69416e755db486910eb36eee5dba5
Bill Lin <billlin@BilldeMacBook-Pro.local>
2017-09-06 16:47:11 +0800

    delete irrelevant code

--- src/Mine/asd.java
+++
@@ -1,9 +0,0 @@
-package Mine;
-
-public class asd {
-    public static void main(String[] args) {
-        String str = "// ^ ^ ^/^n";
-        int a = str.length();
-        System.out.println(3/2);
-    }
-}
